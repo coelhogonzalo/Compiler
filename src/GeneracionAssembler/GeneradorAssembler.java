a664c633bd6bebb7027b2d998580f0e66e8e3f5f
Gonzalo Coelho <coelhogonza@gmail.com>
2018-11-18 00:11:10 -0300

    Agregue el \r a cada \n

--- src/GeneracionAssembler/GeneradorAssembler.java
+++ src/GeneracionAssembler/GeneradorAssembler.java
@@ -20,17 +20,17 @@ import GeneracionCodigoIntermedio.Polaca_Inversa;
 public class GeneradorAssembler {
     private Stack<StringBuilder> pilaVar = new Stack<StringBuilder>();
     Polaca_Inversa PI = null;
-    StringBuilder codigo = new StringBuilder("\n");
+    StringBuilder codigo = new StringBuilder("\r\n");
     StringBuilder inicio = new StringBuilder();
     public int contador = 0;
-    StringBuilder funciones = new StringBuilder("\n");
-    StringBuilder declaracion = new StringBuilder("\n");
+    StringBuilder funciones = new StringBuilder("\r\n");
+    StringBuilder declaracion = new StringBuilder("\r\n");
     boolean estaEnFuncion = false;
     private List<Integer> saltitos = new ArrayList<Integer>();
 	StringBuilder signo = new StringBuilder();
 	StringBuilder aux_parametro = new StringBuilder();
 	StringBuilder aux_param_declarada = new StringBuilder();
-	StringBuilder fin = new StringBuilder("\n");
+	StringBuilder fin = new StringBuilder("\r\n");
 	StringBuilder primerComparado = new StringBuilder();
 	StringBuilder segundoComparado = new StringBuilder();
 	//Set<StringBuilder> variables = new HashSet<StringBuilder>();
@@ -63,9 +63,9 @@ public class GeneradorAssembler {
     		
     		if(saltitos.contains(i)){
 				if(!estaEnFuncion)
-					codigo.append("Label"+i+":"+"\n");
+					codigo.append("Label"+i+":"+"\r\n");
 				else
-					funciones.append("Label"+i+":"+"\n");
+					funciones.append("Label"+i+":"+"\r\n");
     		}
 			
     		if(PI.getPI().get(i).charAt(0) == '_'){
@@ -81,8 +81,8 @@ public class GeneradorAssembler {
 				pilaVar.pop();
 				StringBuilder nom_fun = pilaVar.pop();
 				aux_param_declarada = pilaVar.pop();
-				funciones.append("@FUNCTION_"+nom_fun+"\n");
-				funciones.append("MOV EAX, @aux_param"+"\n"+"MOV "+aux_param_declarada+", EAX"+"\n"); //asigno el valor del parametro real al de la funcion
+				funciones.append("@FUNCTION_"+nom_fun+"\r\n");
+				funciones.append("MOV EAX, @aux_param"+"\r\n"+"MOV "+aux_param_declarada+", EAX"+"\r\n"); //asigno el valor del parametro real al de la funcion
 				estaEnFuncion = true; //HAGO QUE EMPIEZE A ESCRIBIR EN LA PARTE DE FUNCIONES
 			}
 			else{
@@ -93,12 +93,12 @@ public class GeneradorAssembler {
 					System.out.println("Retorno "+retorno);
 					Token t = null; 
 						if(Analizador_Lexico.tablaSimbolos.get(retorno.toString()).tipo.equals("uslinteger")){
-								funciones.append("MOV EAX, " +retorno + "\n" + "MOV @aux_fun, EAX" +"\n" +"RET" +"\n \n");
+								funciones.append("MOV EAX, " +retorno + "\r\n" + "MOV @aux_fun, EAX" +"\r\n" +"RET" +"\r\n \r\n");
 								t = new Token("@aux_fun",Analizador_Lexico.TOKEN_UL,"uslinteger"); //VER
 						}
 						
 						if(Analizador_Lexico.tablaSimbolos.get(retorno.toString()).tipo.equals("single")){
-								funciones.append("FLD "+retorno+ "\n" + "FSTP @aux_fun"+"\n" +"RET" +"\n \n");
+								funciones.append("FLD "+retorno+ "\r\n" + "FSTP @aux_fun"+"\r\n" +"RET" +"\r\n \r\n");
 								t = new Token("@aux_fun",Analizador_Lexico.TOKEN_FLOAT,"single"); //VER
 						}//ACA ARRIBA SAQUE retorno.toString() y lo reemplaze por "@aux_fun"
 					
@@ -128,15 +128,15 @@ public void generarCodigoAssembler(StringBuilder escritura){
 		StringBuilder asignacion = desapilar();
 		//Token t = null;
 		if((Analizador_Lexico.tablaSimbolos.get(aAsignar.toString()).tipo.equals("uslinteger"))&&(Analizador_Lexico.tablaSimbolos.get(asignacion.toString()).tipo.equals("uslinteger"))){
-			escritura.append("MOV EAX, "+asignacion+"\n"+"MOV "+aAsignar+", EAX"+"\n" ); //ver si va lo de mov @aux0, EAX despues de esto
+			escritura.append("MOV EAX, "+asignacion+"\r\n"+"MOV "+aAsignar+", EAX"+"\r\n" ); //ver si va lo de mov @aux0, EAX despues de esto
 		}
 		else{
 			System.out.println("A asignar: "+aAsignar+"  asignacion: "+asignacion.toString());
 			if((Analizador_Lexico.tablaSimbolos.get(aAsignar.toString()).tipo.equals("single"))&&(Analizador_Lexico.tablaSimbolos.get(asignacion.toString()).tipo.equals("single"))){
-					escritura.append("FLD "+asignacion+"\n"+"FSTP "+ aAsignar+"\n"); //creo que seria FLD asignacion + FSTP aAsignar
+					escritura.append("FLD "+asignacion+"\r\n"+"FSTP "+ aAsignar+"\r\n"); //creo que seria FLD asignacion + FSTP aAsignar
 			}
 			else 
-				escritura.append("JMP @LABEL_TIPOS_DISTINTOS"+"\n");
+				escritura.append("JMP @LABEL_TIPOS_DISTINTOS"+"\r\n");
 		}
 
 	}
@@ -148,8 +148,8 @@ public void generarCodigoAssembler(StringBuilder escritura){
 			pilaVar.pop(); //SACO EL CALL
 			StringBuilder fun_llamada = pilaVar.pop();
 			aux_parametro = pilaVar.pop(); //asigno el valor del parametro
-			escritura.append("MOV EAX, "+aux_parametro+"\n"+"MOV @aux_param"+", EAX"+"\n"); //asigno el valor del parametro real al de la funcion
-			escritura.append("CALL @FUNCTION_"+fun_llamada+"\n");
+			escritura.append("MOV EAX, "+aux_parametro+"\r\n"+"MOV @aux_param"+", EAX"+"\r\n"); //asigno el valor del parametro real al de la funcion
+			escritura.append("CALL @FUNCTION_"+fun_llamada+"\r\n");
 		}
 		else{	
 			//PARA COMPARACIONES	
@@ -160,22 +160,22 @@ public void generarCodigoAssembler(StringBuilder escritura){
 				 System.out.println("Voy a comparar '"+primerComparado+"' con '"+segundoComparado+"'");
 				//AMBOS INTEGER
 				if((Analizador_Lexico.tablaSimbolos.get(primerComparado.toString()).tipo.equals("uslinteger")) && (Analizador_Lexico.tablaSimbolos.get(segundoComparado.toString()).tipo.equals("uslinteger"))){
-					escritura.append("MOV EAX, "+primerComparado+"\n"+"MOV EBX, "+segundoComparado+"\n"+"CMP EAX,EBX"+"\n");
+					escritura.append("MOV EAX, "+primerComparado+"\r\n"+"MOV EBX, "+segundoComparado+"\r\n"+"CMP EAX,EBX"+"\r\n");
 					Token t = new Token("@aux"+contador,Analizador_Lexico.TOKEN_UL,"uslinteger");
 					Analizador_Lexico.tablaSimbolos.put(t.lexema,t);
 				}
 				//AMBOS FLOAT 
 				if((Analizador_Lexico.tablaSimbolos.get(primerComparado.toString()).tipo.equals("single")) && ((Analizador_Lexico.tablaSimbolos.get(segundoComparado.toString()).tipo.equals("single")))){
-					escritura.append("FLD "+primerComparado+"\n"+"FLD "+segundoComparado+"\n"+"FCOM"+"\n");//+"FSTSW @aux"+contador+"\n"+"MOV EAX, @aux"+contador+"\n"+"SAHF"+"\n");
+					escritura.append("FLD "+primerComparado+"\r\n"+"FLD "+segundoComparado+"\r\n"+"FCOM"+"\r\n");//+"FSTSW @aux"+contador+"\r\n"+"MOV EAX, @aux"+contador+"\r\n"+"SAHF"+"\r\n");
 					Token t = new Token("@aux"+contador,Analizador_Lexico.TOKEN_FLOAT,"single");
 					Analizador_Lexico.tablaSimbolos.put(t.lexema,t);
 				}
 				//DISTINTOS TIPOS
 				Token t=null;
 				if((Analizador_Lexico.tablaSimbolos.get(primerComparado.toString()).tipo.equals("uslinteger")) && ((Analizador_Lexico.tablaSimbolos.get(segundoComparado.toString()).tipo.equals("single"))))
-					escritura.append("JMP @LABEL_TIPOS_DISTINTOS"+"\n");
+					escritura.append("JMP @LABEL_TIPOS_DISTINTOS"+"\r\n");
 				if((Analizador_Lexico.tablaSimbolos.get(primerComparado.toString()).tipo.equals("single")) && ((Analizador_Lexico.tablaSimbolos.get(segundoComparado.toString()).tipo.equals("uslinteger"))))
-					escritura.append("JMP @LABEL_TIPOS_DISTINTOS"+"\n");
+					escritura.append("JMP @LABEL_TIPOS_DISTINTOS"+"\r\n");
 				
 				StringBuilder aux= new StringBuilder("@aux"+contador);
 				//No habria que agregar a la ts la aux aca? o mas arriba cuando sabes si es un single o un uslinteger
@@ -189,27 +189,27 @@ public void generarCodigoAssembler(StringBuilder escritura){
 				StringBuilder label = pilaVar.pop();
 				System.out.println("el label es: "+label);
 				if(Integer.parseInt(label.substring(5,label.length()))>=PI.getPI().size()) //VERIFICO, SI NO CUMPLE LLEVA AL FINAL //ACA CREO ES 5 NO 6
-					label =new StringBuilder("@LABEL_END"+"\n");
+					label =new StringBuilder("@LABEL_END"+"\r\n");
 				
 				if(signo.toString().equals("="))
-						escritura.append("JNE "+label+"\n");
+						escritura.append("JNE "+label+"\r\n");
 					
 				if(signo.toString().equals("!="))
-						escritura.append("JE "+label+"\n");
+						escritura.append("JE "+label+"\r\n");
 					
 				if(Analizador_Lexico.tablaSimbolos.get(primerComparado.toString()).tipo.equals("uslinteger")){
 					if(signo.toString().equals("<"))
-						escritura.append("JGL "+label+"\n");
+						escritura.append("JGL "+label+"\r\n");
 					
 					if(signo.toString().equals(">"))
-						escritura.append("JLE "+label+"\n");
+						escritura.append("JLE "+label+"\r\n");
 				}
 				if(Analizador_Lexico.tablaSimbolos.get(primerComparado.toString()).tipo.equals("single")){
 					if(signo.toString().equals("<"))
-						escritura.append("JAE "+label+"\n");
+						escritura.append("JAE "+label+"\r\n");
 					
 					if(signo.toString().equals(">"))
-						escritura.append("JBE "+label+"\n");
+						escritura.append("JBE "+label+"\r\n");
 				}
 			}
 			else{
@@ -217,8 +217,8 @@ public void generarCodigoAssembler(StringBuilder escritura){
 					pilaVar.pop(); //SACO EL BT
 					StringBuilder label = pilaVar.pop();
 					if(Integer.parseInt(label.substring(5,label.length()))>=PI.getPI().size()) //VERIFICO SI ESTO LLEVA AL FINAL
-						label =new StringBuilder("@LABEL_END"+"\n");
-					escritura.append("JMP "+label+"\n");
+						label =new StringBuilder("@LABEL_END"+"\r\n");
+					escritura.append("JMP "+label+"\r\n");
 				}	
 				else{
 					//PARA PRINTS
@@ -226,14 +226,14 @@ public void generarCodigoAssembler(StringBuilder escritura){
 					pilaVar.pop(); //SACO LA PALABRA PRINT
 						if(pilaVar.peek().toString().contains("'")){
 							StringBuilder mensajito = pilaVar.pop();
-							escritura.append("invoke MessageBox, NULL, addr "+mensajito+", addr "+mensajito+", MB_OK"+"\n");
+							escritura.append("invoke MessageBox, NULL, addr "+mensajito+", addr "+mensajito+", MB_OK"+"\r\n");
 						}
 						else{
 							StringBuilder variable = pilaVar.pop();
 							if(Analizador_Lexico.tablaSimbolos.get(variable.toString()).tipo.equals("single"))
-								escritura.append("invoke printf, cfm$(%.20Lf\\n), "+variable+"\n");
+								escritura.append("invoke printf, cfm$(%.20Lf\\r\n), "+variable+"\r\n");
 							if(Analizador_Lexico.tablaSimbolos.get(variable.toString()).tipo.equals("uslinteger"))
-								escritura.append("invoke printf, cfm$(%.%llu\\n), "+variable+"\n");
+								escritura.append("invoke printf, cfm$(%.%llu\\r\n), "+variable+"\r\n");
 						}
 					}
 					else{
@@ -248,9 +248,9 @@ public void generarCodigoAssembler(StringBuilder escritura){
 							if((Analizador_Lexico.tablaSimbolos.get(primerOperando.toString()).tipo.equals("single")) && ((Analizador_Lexico.tablaSimbolos.get(segundoOperando.toString()).tipo.equals("single"))))
 								generarCodigoSingle(operador,primerOperando,segundoOperando,escritura);
 							if((Analizador_Lexico.tablaSimbolos.get(primerOperando.toString()).tipo.equals("uslinteger")) && ((Analizador_Lexico.tablaSimbolos.get(segundoOperando.toString()).tipo.equals("single"))))
-								escritura.append("JMP @LABEL_END"+"\n");
+								escritura.append("JMP @LABEL_END"+"\r\n");
 							if((Analizador_Lexico.tablaSimbolos.get(primerOperando.toString()).tipo.equals("single")) && ((Analizador_Lexico.tablaSimbolos.get(segundoOperando.toString()).tipo.equals("uslinteger"))))
-								escritura.append("JMP @LABEL_END"+"\n");
+								escritura.append("JMP @LABEL_END"+"\r\n");
 						}
 					}
 				}
@@ -264,22 +264,22 @@ public void generarCodigoInteger(StringBuilder operador,StringBuilder primerOper
     	String op = operador.toString();
     	StringBuilder aux = null;
     	switch(op){
-    	case "+" :  escritura.append("MOV EAX,"+primerOperando+"\n"+"ADD EAX, "+segundoOperando+"\n"+"MOV "+"@aux"+contador+", EAX"+"\n");
+    	case "+" :  escritura.append("MOV EAX,"+primerOperando+"\r\n"+"ADD EAX, "+segundoOperando+"\r\n"+"MOV "+"@aux"+contador+", EAX"+"\r\n");
     				aux= new StringBuilder("@aux"+contador);
     				pilaVar.push(aux);  				
     				
     	break;
-    	case"-" : escritura.append("MOV EAX,"+segundoOperando+"\n"+"CMP EAX, "+primerOperando+"\n"+"JL @LABEL_RESUL_NEG"+"\n"+"SUB EAX, "+primerOperando+"\n"+ "MOV "+"@aux"+contador+", EAX"+"\n");
+    	case"-" : escritura.append("MOV EAX,"+segundoOperando+"\r\n"+"CMP EAX, "+primerOperando+"\r\n"+"JL @LABEL_RESUL_NEG"+"\r\n"+"SUB EAX, "+primerOperando+"\r\n"+ "MOV "+"@aux"+contador+", EAX"+"\r\n");
   				  aux= new StringBuilder("@aux"+contador);
   				  pilaVar.push(aux);
 		 		 
     	break;
-    	case"*": escritura.append("MOV EAX,"+primerOperando+"\n"+"MUL EAX, "+segundoOperando+"\n"+"JO @LABEL_OVF"+"\n"+ "MOV "+"@aux"+contador+", EAX"+"\n");
+    	case"*": escritura.append("MOV EAX,"+primerOperando+"\r\n"+"MUL EAX, "+segundoOperando+"\r\n"+"JO @LABEL_OVF"+"\r\n"+ "MOV "+"@aux"+contador+", EAX"+"\r\n");
  				 aux= new StringBuilder("@aux"+contador);
  				 pilaVar.push(aux);
 		break;		
 
-    	case"/" : escritura.append("MOV EAX,"+segundoOperando+"\n"+"MOV EBX,"+primerOperando+"\n"+"CMP EBX, 0.0"+"\n"+"JZ @LABEL_ZERO"+"\n"+"DIV EAX, "+primerOperando+"\n"+ "MOV "+"@aux"+contador+", EAX"+"\n");
+    	case"/" : escritura.append("MOV EAX,"+segundoOperando+"\r\n"+"MOV EBX,"+primerOperando+"\r\n"+"CMP EBX, 0.0"+"\r\n"+"JZ @LABEL_ZERO"+"\r\n"+"DIV EAX, "+primerOperando+"\r\n"+ "MOV "+"@aux"+contador+", EAX"+"\r\n");
  				  aux= new StringBuilder("@aux"+contador);
  				  pilaVar.push(aux);
 	
@@ -294,23 +294,23 @@ public void generarCodigoSingle(StringBuilder operador,StringBuilder primerOpera
 			String op = operador.toString();
 			StringBuilder aux = null;
 			switch(op){
-			case "+" :  escritura.append("FLD "+primerOperando+"\n"+"FLD "+segundoOperando+"\n"+ "FADD"+"\n"+"FSTP "+"@aux"+contador+"\n");
+			case "+" :  escritura.append("FLD "+primerOperando+"\r\n"+"FLD "+segundoOperando+"\r\n"+ "FADD"+"\r\n"+"FSTP "+"@aux"+contador+"\r\n");
 						aux= new StringBuilder("@aux"+contador);
 						pilaVar.push(aux);
 					
 			break;
-			case"-" : escritura.append("FLD "+primerOperando+"\n"+"FLD "+segundoOperando+"\n"+"FCOM"+"\n"+"JB @LABEL_RESUL_NEG"+"\n"+ "FSUB"+"\n"+"FSTP "+"@aux"+contador+"\n");
+			case"-" : escritura.append("FLD "+primerOperando+"\r\n"+"FLD "+segundoOperando+"\r\n"+"FCOM"+"\r\n"+"JB @LABEL_RESUL_NEG"+"\r\n"+ "FSUB"+"\r\n"+"FSTP "+"@aux"+contador+"\r\n");
 					  aux= new StringBuilder("@aux"+contador);
 					  pilaVar.push(aux);
 					  
 			break;
-			case"*": escritura.append("FLD "+primerOperando+"\n"+"FLD "+segundoOperando+"\n"+ "FMUL"+"\n"+"FSTP "+"@aux"+contador+"\n"+"FLD cte_max_rango"+"\n"+"FLD @aux"+contador+"\n"+"FCOM"+"\n"+"FSTSW AX"+"\n"+"SAHF"+"\n"+"JA @LABEL_OVF"+"\n");
+			case"*": escritura.append("FLD "+primerOperando+"\r\n"+"FLD "+segundoOperando+"\r\n"+ "FMUL"+"\r\n"+"FSTP "+"@aux"+contador+"\r\n"+"FLD cte_max_rango"+"\r\n"+"FLD @aux"+contador+"\r\n"+"FCOM"+"\r\n"+"FSTSW AX"+"\r\n"+"SAHF"+"\r\n"+"JA @LABEL_OVF"+"\r\n");
 					 aux= new StringBuilder("@aux"+contador);
 					 pilaVar.push(aux);		
 					 
 			break;		 
 
-			case"/" :escritura.append("FLD "+segundoOperando+"\n"+"FLD "+primerOperando+"\n"+"FCOM 0"+"\n"+"JE @LABEL_ZERO"+"\n"+ "FDIV"+"\n"+"FSTP "+"@aux"+contador+"\n");
+			case"/" :escritura.append("FLD "+segundoOperando+"\r\n"+"FLD "+primerOperando+"\r\n"+"FCOM 0"+"\r\n"+"JE @LABEL_ZERO"+"\r\n"+ "FDIV"+"\r\n"+"FSTP "+"@aux"+contador+"\r\n");
 					 aux= new StringBuilder("@aux"+contador);
 					 pilaVar.push(aux);
 					
@@ -330,37 +330,37 @@ public void generarCodigoSingle(StringBuilder operador,StringBuilder primerOpera
 
     public void generarDeclaracion(){
 
-    	declaracion.append("mensaje_overflow db \"Hubo overflow uwu\", 0 "+"\n");
-    	declaracion.append("mensaje_zero db \"El divisor es 0 huehuehue\", 0 "+"\n");
-    	declaracion.append("mensaje_resultadoNeg db \"Me dio negativo xD\", 0 "+"\n");
-    	declaracion.append("@aux_fun dd?"+"\n");
-    	declaracion.append("@aux_param dd?"+"\n");
+    	declaracion.append("mensaje_overflow db \"Hubo overflow uwu\", 0 "+"\r\n");
+    	declaracion.append("mensaje_zero db \"El divisor es 0 huehuehue\", 0 "+"\r\n");
+    	declaracion.append("mensaje_resultadoNeg db \"Me dio negativo xD\", 0 "+"\r\n");
+    	declaracion.append("@aux_fun dd?"+"\r\n");
+    	declaracion.append("@aux_param dd?"+"\r\n");
     	for(int i=0; i<contador; i++){
-    		declaracion.append("@aux"+i+" dd?"+"\n");
+    		declaracion.append("@aux"+i+" dd?"+"\r\n");
     	}
     	
     	for(int j=0; j<variables.size(); j++)
-    		declaracion.append(variables.get(j)+" dd?"+"\n");
+    		declaracion.append(variables.get(j)+" dd?"+"\r\n");
         //recorrer todo al final para ver cuales se usaron y cuales no, alto rip
-        declaracion.append(".const "+"\n"+" cte_max_rango  equ 3.40282347E+38"+"\n");
-    	declaracion.append(".code"+"\n"+"start: ");
+        declaracion.append(".const "+"\r\n"+" cte_max_rango  equ 3.40282347E+38"+"\r\n");
+    	declaracion.append(".code"+"\r\n"+"start: ");
     }
    
  
     public void generarEncabezado(){
-    	inicio.append(".386"+"\n"+".model flat, stdcall"+"\n"+"option casemap :none "+"\n"+"include \\masm32\\include\\windows.inc"+"\n"+"include \\masm32\\include\\kernel32.inc"
-    					+"\n"+"include \\masm32\\include\\masm32.inc"+"\n"+"includelib \\masm32\\lib\\kernel32.lib"+"\n"+"includelib \\masm32\\lib\\masm32.lib"+"\n"
-    			+"\n"+"include \\masm32\\include\\masm32rt.inc"+"\n"+"dll_dllcrt0 PROTO C"+"\n"+"printf PROTO C: VARARG"+"\n"+".data"+"\n");   
+    	inicio.append(".386"+"\r\n"+".model flat, stdcall"+"\r\n"+"option casemap :none "+"\r\n"+"include \\masm32\\include\\windows.inc"+"\r\n"+"include \\masm32\\include\\kernel32.inc"
+    					+"\r\n"+"include \\masm32\\include\\masm32.inc"+"\r\n"+"includelib \\masm32\\lib\\kernel32.lib"+"\r\n"+"includelib \\masm32\\lib\\masm32.lib"+"\r\n"
+    			+"\r\n"+"include \\masm32\\include\\masm32rt.inc"+"\r\n"+"dll_dllcrt0 PROTO C"+"\r\n"+"printf PROTO C: VARARG"+"\r\n"+".data"+"\r\n");   
     }
     
     public void generarMensajitosDeControl(){
-    	fin.append("@LABEL_OVF:"+ "\n"+ "invoke MesseageBox,NULL,addr mensaje_overflow,addr"+"\n"+"mensaje_overflow,MB_OK"+"\n"+"JMP @LABEL_END"+"\n");
-    	fin.append("@LABEL_ZERO:"+ "\n"+ "invoke MesseageBox,NULL,addr mensaje_zero,addr"+"\n"+"mensaje_zero,MB_OK"+"\n"+"JMP @LABEL_END"+"\n");
-    	fin.append("@LABEL_RESUL_NEG:"+ "\n"+ "invoke MesseageBox,NULL,addr mensaje_resultadoNeg,addr"+"\n"+"mensaje_resultadoNeg,MB_OK"+"\n"+"JMP @LABEL_END"+"\n");
+    	fin.append("@LABEL_OVF:"+ "\r\n"+ "invoke MesseageBox,NULL,addr mensaje_overflow,addr"+"\r\n"+"mensaje_overflow,MB_OK"+"\r\n"+"JMP @LABEL_END"+"\r\n");
+    	fin.append("@LABEL_ZERO:"+ "\r\n"+ "invoke MesseageBox,NULL,addr mensaje_zero,addr"+"\r\n"+"mensaje_zero,MB_OK"+"\r\n"+"JMP @LABEL_END"+"\r\n");
+    	fin.append("@LABEL_RESUL_NEG:"+ "\r\n"+ "invoke MesseageBox,NULL,addr mensaje_resultadoNeg,addr"+"\r\n"+"mensaje_resultadoNeg,MB_OK"+"\r\n"+"JMP @LABEL_END"+"\r\n");
     }
     
     public void generarFin(){
-    	fin.append("@LABEL_END:"+"\n"+"invoke ExitProcess, 0"+"\n"+"end start");
+    	fin.append("@LABEL_END:"+"\r\n"+"invoke ExitProcess, 0"+"\r\n"+"end start");
     }
     
 //Cosas que me dijo anto: recorrer toda la polaca para crear la declaracion de variable, osea hacerlo al final
